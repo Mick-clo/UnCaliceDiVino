6e8eba8aaea55f2063668a9d034a86bf79ca686e
Timeea Samoila <timeeasamoila12@gmail.com>
2024-10-31 16:39:22 +0100

    footer and config device

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 end
+
+class PagesController < ApplicationController
+  skip_before_action :authenticate_user!, only: :home
+
+  def home
+  end
+end
